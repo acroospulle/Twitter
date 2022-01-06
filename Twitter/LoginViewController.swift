3778871835b717664b0ee17f1e9970d9fe90dd3a
acroospulle <Ashni.Croospulle@cix.csi.cuny.edu>
2022-01-06 10:18:59 -0500

    to be able to login

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -18,7 +18,14 @@ class LoginViewController: UIViewController {
     
 
     @IBAction func onLoginButton(_ sender: Any) {
-        TwitterAPICaller.client?.login(url: <#T##String#>, success: <#T##() -> ()#>, failure: <#T##(Error) -> ()#>)
+        
+        let myUrl = "https://api.twitter.com/oauth/request_token"
+        
+        TwitterAPICaller.client?.login(url: myUrl, success: {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { Error in
+            print("Could not log in!")
+        })
         
     }
     
