3fa3c821a0da305ba2ffd09f86ad21e2655e0eff
acroospulle <Ashni.Croospulle@cix.csi.cuny.edu>
2022-01-06 11:42:39 -0500

    created  HomeTableViewController.swift file

---
+++ Twitter/HomeTableViewController.swift
@@ -0,0 +1,34 @@
+//
+//  HomeTableViewController.swift
+//  Twitter
+//
+//  Created by Ashni Croospulle on 1/6/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    @IBAction func onLogout(_ sender: Any) {
+    }
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    
+}
