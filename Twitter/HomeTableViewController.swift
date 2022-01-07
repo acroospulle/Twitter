227d70161a00edb44e4cea5a4e0ce535ee118458
acroospulle <Ashni.Croospulle@cix.csi.cuny.edu>
2022-01-07 14:18:10 -0500

    removed the tweetfile

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -18,12 +18,17 @@ class HomeTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweets()
+       
         
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        loadTweets()
+        
+    }
     
     @objc func loadTweets() {
         numberOfTweet = 20
