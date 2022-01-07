b3e4cb72eeb9786699f0182663cb8c10422dbbe6
acroospulle <Ashni.Croospulle@cix.csi.cuny.edu>
2022-01-07 15:44:17 -0500

    cancel function

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -17,14 +17,13 @@ class TweetViewController: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
     }
-    */
-
+    
+    
+    @IBAction func tweet(_ sender: Any) {
+    }
+    
 }
