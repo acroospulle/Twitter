9e1bcbd2a8acf689f0d6d8df21713601834fcb56
acroospulle <Ashni.Croospulle@cix.csi.cuny.edu>
2022-01-08 15:31:50 -0500

    tweet function

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -10,8 +10,11 @@ import UIKit
 
 class TweetViewController: UIViewController {
 
+    @IBOutlet weak var tweetTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tweetTextView.becomeFirstResponder()
 
         // Do any additional setup after loading the view.
     }
@@ -24,6 +27,17 @@ class TweetViewController: UIViewController {
     
     
     @IBAction func tweet(_ sender: Any) {
+        if (!tweetTextView.text.isEmpty) {
+            TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: {
+                self.dismiss(animated: true,completion: nil)
+            }, failure: { (error) in
+                print("Error posting tweet \(error)")
+                self.dismiss(animated: true, completion: nil)
+            })
+        } else {
+            self.dismiss(animated: true,completion: nil)
+        }
+           
     }
     
 }
