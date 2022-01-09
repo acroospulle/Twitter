f2caa7253147b6f6cb9226b17372f6547e441eba
acroospulle <Ashni.Croospulle@cix.csi.cuny.edu>
2022-01-09 13:17:47 -0500

    func favoriteTweet and func retweet

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -22,6 +22,8 @@ class HomeTableViewController: UITableViewController {
         
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
     }
     
     override func viewDidAppear(_ animated: Bool) {
--- Twitter/TweetCellTableViewCell.swift
+++ Twitter/TweetCellTableViewCell.swift
@@ -17,6 +17,25 @@ class TweetCellTableViewCell: UITableViewCell {
     
     @IBOutlet weak var tweetContent: UILabel!
     
+    @IBOutlet weak var retweetButton: UIButton!
+    
+    @IBOutlet weak var favButton: UIButton!
+    
+    
+    
+    @IBAction func favoriteTweet(_ sender: Any) {
+    }
+    
+    
+    
+    @IBAction func retweet(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
